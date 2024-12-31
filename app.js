5a7c7e5e9912dbc318a4e7982fefa1d9db6b4522
aneebamoezz <anibakaleem@gmail.com>
2024-12-31 13:02:18 +0500

    Update files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Question: Write a function that takes two numbers as arguments and returns their sum. If no arguments are provided, return 0.
 
 function sumNumber(a = 0, b = 0) {
-    return a + b
+  return a + b;
 }
 console.log(sumNumber(10 + 40));
 console.log(sumNumber(40 + 70));
@@ -13,16 +13,15 @@ const btn = document.querySelector("#my-btn");
 const body = document.querySelector("body");
 
 btn.addEventListener("click", () => {
-  const randomColor = `#${Math.floor(Math.random() * 23456543)}`
+  const randomColor = `#${Math.floor(Math.random() * 23456543)}`;
   document.body.style.backgroundColor = randomColor;
 });
 
-
 //Question: Build a counter that increments or decrements a number when respective buttons are clicked.
-let counter = document.querySelector("#counter")
-let inBtn = document.querySelector("#increment")
-let deBtn = document.querySelector("#decrement")
-let resetBtn = document.querySelector("#reset")
+let counter = document.querySelector("#counter");
+let inBtn = document.querySelector("#increment");
+let deBtn = document.querySelector("#decrement");
+let resetBtn = document.querySelector("#reset");
 
 let counts = 0;
 
@@ -31,18 +30,66 @@ function countUpdat() {
 }
 
 inBtn.addEventListener("click", () => {
- counts++
- countUpdat()
-})
+  counts++;
+  countUpdat();
+});
 
 deBtn.addEventListener("click", () => {
- counts--
- countUpdat()
-})
+  counts--;
+  countUpdat();
+});
 
 resetBtn.addEventListener("click", () => {
-  counts = 0
-  countUpdat()
- })
- 
- 
+  counts = 0;
+  countUpdat();
+});
+
+//Question: Given an array of numbers, write a function that filters out odd numbers and returns only even numbers.
+
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+let newArr = nums.filter((val) => {
+  return val % 2 === 0;
+});
+
+console.log(newArr);
+
+//Question: Fetch a random joke from an Array of jokes and display it in a <p> tag when a button is clicked.
+
+import { fakeJokes } from "./data.js";
+
+
+let jokeBtn = document.getElementById("jokeButton");
+const jokeDisplay = document.getElementById("jokeDisplay");
+
+jokeBtn.addEventListener("click", () => {
+  console.log("click");
+  const myStr = fakeJokes[Math.floor(Math.random() * fakeJokes.length)];
+  jokeDisplay.innerHTML = `<strong>Q: ${myStr.question}</strong>  <br> <strong>A:</strong> ${myStr.answer}`;
+});
+
+//Question: Write a function that divides two numbers and handles the error if the divisor is 0.
+
+function divideNums(a, b) {
+  if (b === 0) {
+    return "Error the divisor is 0";
+  } else {
+    return a / b
+  }
+}
+
+console.log(divideNums(2, 0))
+
+//Question: Save a user's name in localStorage and display it when they revisit the page.
+
+let myLocalStorageName = document.getElementById("myName")
+localStorage.setItem("name", "Aneeba")
+
+
+
+const getItems = localStorage.getItem("name")
+
+myLocalStorageName.innerText = getItems;
+
+//decimal
+
---
+++ data.js
@@ -0,0 +1,92 @@
+export const fakeJokes = [
+  {
+    id: 3,
+    question: "Why did the bicycle fall over?",
+    answer: "Because it was two-tired!",
+  },
+  {
+    id: 4,
+    question: "What do you call cheese that isn't yours?",
+    answer: "Nacho cheese!",
+  },
+  {
+    id: 5,
+    question: "Why did the golfer bring two pairs of pants?",
+    answer: "In case he got a hole in one!",
+  },
+  {
+    id: 6,
+    question: "Why did the tomato turn red?",
+    answer: "Because it saw the salad dressing!",
+  },
+  {
+    id: 7,
+    question: "Why do bees have sticky hair?",
+    answer: "Because they use honeycombs.",
+  },
+  {
+    id: 8,
+    question: "What do you call fake spaghetti?",
+    answer: "An impasta!",
+  },
+  {
+    id: 9,
+    question: "Why can’t your nose be 12 inches long?",
+    answer: "Because then it would be a foot!",
+  },
+  {
+    id: 10,
+    question: "Why did the math book look sad?",
+    answer: "It had too many problems.",
+  },
+  {
+    id: 11,
+    question: "What do you call a bear with no teeth?",
+    answer: "A gummy bear!",
+  },
+  {
+    id: 12,
+    question: "Why don’t oysters donate to charity?",
+    answer: "Because they’re shellfish.",
+  },
+  {
+    id: 13,
+    question: "Why did the cookie go to the doctor?",
+    answer: "Because it felt crumby.",
+  },
+  {
+    id: 14,
+    question: "What did one wall say to the other wall?",
+    answer: "I’ll meet you at the corner.",
+  },
+  {
+    id: 15,
+    question: "Why did the computer go to the therapist?",
+    answer: "It had too many bytes!",
+  },
+  {
+    id: 16,
+    question: "What did the big flower say to the little flower?",
+    answer: "Hey, bud!",
+  },
+  {
+    id: 17,
+    question: "Why did the coffee file a police report?",
+    answer: "It got mugged!",
+  },
+  {
+    id: 18,
+    question: "Why did the gym close down?",
+    answer: "It just didn’t work out.",
+  },
+  {
+    id: 19,
+    question: "Why are ghosts bad liars?",
+    answer: "Because you can see right through them!",
+  },
+  {
+    id: 20,
+    question: "Why don’t eggs tell jokes?",
+    answer: "Because they might crack up!",
+  },
+];
